d1c67397e4d47b9d429df50cf5a14903a43a6f94
Matthias Haeussler <maeddes@gmail.com>
2019-09-14 21:37:16 +0200

    basic rest controller and todo entity

--- todolist/src/main/java/de/maeddes/todolist/TodolistApplication.java
+++ todolist/src/main/java/de/maeddes/todolist/TodolistApplication.java
@@ -1,7 +1,12 @@
 package de.maeddes.todolist;
 
+import javax.persistence.Entity;
+import javax.persistence.Id;
+
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.data.repository.CrudRepository;
+import org.springframework.data.rest.core.annotation.RepositoryRestResource;
 
 @SpringBootApplication
 public class TodolistApplication {
@@ -9,5 +14,33 @@ public class TodolistApplication {
 	public static void main(String[] args) {
 		SpringApplication.run(TodolistApplication.class, args);
 	}
+
+}
+
+@Entity
+class Todo{
+
+	@Id
+	String todo;
+
+	public Todo() {
+	}
+
+	public Todo(String todo) {
+		this.todo = todo;
+	}
+
+	public String getTodo() {
+		return todo;
+	}
+
+	public void setTodo(String todo) {
+		this.todo = todo;
+	}
+
+}
+
+@RepositoryRestResource
+interface TodoRepository extends CrudRepository<Todo, String> {
 
 }
